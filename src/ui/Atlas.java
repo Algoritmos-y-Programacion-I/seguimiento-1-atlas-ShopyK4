80c51c3420f6fdf172c75f6aec6d43429ba2a600
Samuel Alejandro Bolanos Ortega <1108564336@u.icesi.edu.co>
2025-02-21 12:58:22 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -16,6 +16,8 @@ public class Atlas {
 
     // Declaracion de constantes (completar)
     final double COSTO_TRAYECTO_AVION = 250000;
+    final double CostoUnaNoche = 150000;
+    final Double CostoTrayectoBus = 80000;
 
 	private Atlas() {
 		escaner = new Scanner(System.in);
@@ -32,8 +34,36 @@ public class Atlas {
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
 
-        // ...
+        int edad;
         System.out.println("Bienvenido " + nombre + "!");
+        System.out.println("Que edad tienes?");
+        edad = escaner.nextInt();
+        
+
+        int cedula;
+        VerSiEsMenorDeEdad(edad);
+
+        cedula = escaner.nextInt();
+        escaner.nextLine();
+
+        String Lugar;
+        System.out.println("Desde donde te comunicas con nosotros: ");
+        Lugar = escaner.nextLine();
+
+        double cantidadnoches;
+        System.out.println("Bienvenido a EVENTOS ATLAS, cuantas noches deseas hospedarte? ");
+        cantidadnoches = escaner.nextInt();
+
+
+        int transporte;
+        System.out.println("Que medio de transporte usaras, selecciona 1 para Avion o 2 para Bus. ");
+        transporte = escaner.nextInt();
+
+
+
+
+
+
         // ...
 
         // Declaracion de salidas calculadas (completar)
@@ -54,11 +84,65 @@ public class Atlas {
 	 * @param 
 	 * @return 
 	 */
+    
+    
+
+
+
+    }
+    
+    
     public double calcularTotalTransporte() {
         return 0; // Completar operacion
     }
+    
+    public static void VerSiEsMenorDeEdad(int edad) {
+        if (edad < 18) {
+            System.out.println("Por favor escribe tu tarjeta de identidad: ");
+            if (edad >= 18) {
+            System.out.println("Por favor escribe tu cedula de ciudadania: ");
+            }
+        }
+    
+    
+
+
+    public static double costodenoches(int costonoches) {
+        double costonoches = CostoUnaNoche * cantidadnoches;
+            return costodenoches;
+    }
+
+   public int tipotransporte(int transporte) {
+        if (transporte == 1) { 
+
+        }
+
+
+    }
+    
+
+
+    
+    
+    
+
+            
+
+
+
+        }
+
+
+    
+    
+    
+
+
+
+
+
+    
 
 	
 
 	
-}
